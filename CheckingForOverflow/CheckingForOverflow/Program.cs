93c99b69254b31cc3de20a8e6e2ee6b3a3e1c613
杨云鹏 <m18012918850@163.com>
2018-12-31 09:57:20 +0800

    the new

---
+++ CheckingForOverflow/CheckingForOverflow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace CheckingForOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //when casting from a long variable to an int variable. If the value stored in a type is too big, it will overflow
+            checked
+            {
+                int x = int.MaxValue - 1;
+                WriteLine(x);
+                x++;
+                WriteLine(x);
+                x++;
+                WriteLine(x);
+                x++;
+                WriteLine(x);
+                //int y = int.MaxValue + 1;
+            }
+            unchecked
+            {
+                int y = int.MaxValue + 1;
+                WriteLine(y);
+                y--;
+                WriteLine(y);
+                y--;
+                WriteLine(y);
+            }
+        }
+    }
+}
