7536904b616979b4516bf823c2aaf0e6b1c75500
杨云鹏 <m18012918850@163.com>
2018-12-31 09:31:35 +0800

    The new

---
+++ HandingExceptions/HandingExceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace HandingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Before parsing");
+            Write("What is your age?");
+            string input = Console.ReadLine();
+            try
+            {
+                int age = int.Parse(input);
+                WriteLine($"You are {age} years old");
+            }
+            //catch(Exception ex)
+            //{
+            //    WriteLine($"{ex.GetType()} says {ex.Message}");
+            //    //Get the type of the exception and the message of the exception
+            //}
+
+            //Catching specific exceptions
+            //there might be other types of exceptions that can occur
+            catch (FormatException)
+            {
+                WriteLine("The age you entered is not a valid number format");
+            }
+
+            catch (OverflowException)
+            {
+                WriteLine("Your age is a valid number format but it is either too big or small.");
+            }
+
+
+            WriteLine("After parsing");
+
+           
+
+
+        }
+    }
+}
