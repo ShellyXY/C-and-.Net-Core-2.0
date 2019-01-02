e5ba13868935afb3ff23d848c188dd3797a59263
杨云鹏 <m18012918850@163.com>
2019-01-03 00:25:24 +0800

    the new chapter6

---
+++ Chapter6/Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Packt.CS7;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var harry = new Person { Name = "Harry" };
+            var mary = new Person { Name = "Mary" };
+            var jill = new Person { Name = "Jill" };
+
+            // call instance method
+            var baby1 = mary.ProcreateWith(harry);
+
+            // call static method
+            var baby2 = Person.Procreate(harry, jill);
+
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine($"{mary.Name}'s first child is named \"{mary.Children[0].Name}\".");
+
+            //Implementing some functionality with an operator
+            //call static operator
+            var baby3 = harry * mary;
+            
+        }
+    }
+}
