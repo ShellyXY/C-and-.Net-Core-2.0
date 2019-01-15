dffbd998979329a97aa3b876c77628d268eecb57
ShellyXY <m18012918850@163.com>
2019-01-15 22:27:42 +0800

    the new chapter09

---
+++ Chapter9/Chapter09/WorkWithDirectories/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkWithDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Chapter9/Chapter09/WorkingWithStreams/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+namespace WorkingWithStreams
+{
+    class Program
+    {
+        //define an array of Viper pilot call signs
+        static string[] callsigns = new string[] { "Husker", "Starbuck", "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack" };
+        static void Main(string[] args)
+        {
+            //define a file to write to 
+            string textFile = Combine(CurrentDirectory, "stream.txt");
+
+            // create a text file and return a helper writer
+            StreamWriter text = File.CreateText(textFile);
+
+
+        }
+    }
+}
