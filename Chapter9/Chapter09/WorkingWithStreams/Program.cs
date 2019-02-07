3169f6a73bcbdcb62460a276487b6fe8023511ca
ShellyXY <m18012918850@163.com>
2019-02-07 22:15:04 +0800

    the new

--- Chapter9/Chapter09/WorkingWithStreams/Program.cs
+++ Chapter9/Chapter09/WorkingWithStreams/Program.cs
@@ -11,7 +11,8 @@ namespace WorkingWithStreams
     {
         //define an array of Viper pilot call signs
         static string[] callsigns = new string[] { "Husker", "Starbuck", "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack" };
-        static void Main(string[] args)
+
+        static void WorkWithText()
         {
             //define a file to write to 
             string textFile = Combine(CurrentDirectory, "stream.txt");
@@ -19,7 +20,80 @@ namespace WorkingWithStreams
             // create a text file and return a helper writer
             StreamWriter text = File.CreateText(textFile);
 
+            //enumerate the strings,writing each one
+            //to the stream on a separate line
+            foreach (string item in callsigns)
+            {
+                text.WriteLine(item);
+            }
+            text.Close();//release resources,the process need to be shut down.
 
+            //output the contexts of the file to the console
+            WriteLine($"{textFile} contains {new FileInfo(textFile).Length} bytes.");
+            WriteLine(File.ReadAllText(textFile));
+        }
+
+        static void WorkWithXML()
+        {
+            FileStream xmlFileStream = null;
+            XmlWriter xml = null;
+            try
+            {
+                //define a file to write to
+                string xmlFile = Combine(CurrentDirectory, "streams.xml");
+
+                //create a file streams
+                xmlFileStream = File.Create(xmlFile);
+
+                //Wrap the file stream in an XML writer helper
+                //and automatically indent nested elements
+                xml = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
+
+                //write the XML declration
+                xml.WriteStartDocument();
+
+                //write a root element 
+                xml.WriteStartElement("callsigns");
+
+                //enumerate the strings writing each one to the stream
+                foreach (string item in callsigns)
+                {
+                    xml.WriteElementString("callsign", item);
+                }
+
+                //write the close root element
+                xml.WriteEndElement();
+
+                //close helper and stream
+                xml.Close();
+                xmlFileStream.Close();
+
+                // output all the contents of the file to the Console 
+                WriteLine($"{xmlFile} contains {new FileInfo(xmlFile).Length} bytes.");
+                WriteLine(File.ReadAllText(xmlFile));
+            }
+            catch (Exception ex)
+            {
+                // if the path doesn't exist the exception will be caught
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+            finally
+            {
+                if (xml != null)
+                {
+                    xml.Dispose();
+                    WriteLine("The XML writer's unmanaged resources have been disposed.");
+                }
+                if (xmlFileStream != null)
+                {
+                    xmlFileStream.Dispose();
+                    WriteLine("The file stream's unmanaged resources have been disposed.");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            WorkWithText();
         }
     }
 }
