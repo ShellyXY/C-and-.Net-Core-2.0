2d3e9c4446d2d43e36cfa067fda737b621209497
ShellyXY <m18012918850@163.com>
2019-02-10 21:17:00 +0800

    the new

--- Chapter9/Chapter09/WorkingWithStreams/Program.cs
+++ Chapter9/Chapter09/WorkingWithStreams/Program.cs
@@ -4,6 +4,7 @@ using System.Xml;
 using static System.Console;
 using static System.Environment;
 using static System.IO.Path;
+using System.IO.Compression;
 
 namespace WorkingWithStreams
 {
@@ -91,9 +92,53 @@ namespace WorkingWithStreams
                 }
             }
         }
+        static void WorkWithCompression()
+        {
+            // compress the XML output
+            string gzipFilePath = Combine(CurrentDirectory,"Stream.gzip");
+            FileStream gzipFile = File.Create(gzipFilePath);
+            using (GZipStream compressor = new GZipStream(gzipFile, CompressionMode.Compress))
+            {
+                using (XmlWriter xmlGzip = XmlWriter.Create(compressor))
+                {
+                    xmlGzip.WriteStartDocument();
+                    xmlGzip.WriteStartElement("callsigns");
+                    foreach (string item in callsigns)
+                    {
+                        xmlGzip.WriteElementString("callsigns",item);
+                    }
+                }// also closes the underlying stream 
+
+                // output all the contents of the compressed file to the Console 
+                WriteLine($"{gzipFilePath} contains  { new FileInfo(gzipFilePath).Length} bytes."); 
+                WriteLine(File.ReadAllText(gzipFilePath));
+
+                //read a compressed file
+                WriteLine("Reading the compressed XML File:");
+                gzipFile = File.Open(gzipFilePath, FileMode.Open);
+                using (GZipStream decompressor = new GZipStream(gzipFile, CompressionMode.Decompress))
+                {
+                    using (XmlReader reader = XmlReader.Create(decompressor))
+                    {
+                        while (reader.Read())
+                        {
+                            //check if we are currently on an element node named callsign 
+                            if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "callsign"))
+                            {
+                                reader.Read();//move to the Text node inside the element
+                                WriteLine($"{reader.Value}");// read its value
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            WorkWithText();
+            //WorkWithText();
+            WorkWithXML();
+            WorkWithCompression();
         }
     }
 }
